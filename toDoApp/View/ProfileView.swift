2d252a174846fd385065404b309553d88aebc3f1
ışıl vardarlı <isil.vardarli@ogr.iuc.edu.tr>
2024-11-30 00:29:44 +0300

    profile view and logout

--- toDoApp/View/ProfileView.swift
+++ toDoApp/View/ProfileView.swift
@@ -16,10 +16,36 @@ struct ProfileView: View {
         NavigationView{
             VStack{
                 
+                if let user = profileVM.user{
+                    profile(user: user)
+                }else{
+                    Text("Profile is updating...")
+                }
+                
+                BigButtonView(title: "Logout", action: {
+                    profileVM.logout()}
+                )
             }
             .navigationTitle("Profile")
             
+        }.onAppear{
+            profileVM.fetchUser()
         }
+    }
+    @ViewBuilder
+    func profile(user: User)->some View{
+        Image(systemName: "person.circle")
+            .resizable()
+            .frame(width: 100, height: 100)
+            .foregroundStyle(Color("specialPurple"))
+        
+        VStack{
+            HStack{
+                Text("Email:")
+                Text(user.email)
+            }
+        }
+
     }
 }
 
--- toDoApp/ViewModel/ProfileViewModel.swift
+++ toDoApp/ViewModel/ProfileViewModel.swift
@@ -6,9 +6,35 @@
 //
 
 import Foundation
+import FirebaseAuth
+import FirebaseFirestore
 
 class ProfileViewModel : ObservableObject{
+    @Published var user : User? = nil
     init(){
         
+    }
+    func fetchUser(){
+        guard let userID = Auth.auth().currentUser?.uid else { return }
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(userID).getDocument { (snapshot, error) in
+                guard let data = snapshot?.data(), error == nil else{return}
+                
+                DispatchQueue.main.async {
+                    self.user = User(
+                        id: data["id"] as? String ?? "", email: data["email"] as? String ?? ""
+                    )
+                }
+            }
+    }
+    
+    func logout(){
+        do{
+            try Auth.auth().signOut()
+        }catch{
+            print("error")
+        }
     }
 }
